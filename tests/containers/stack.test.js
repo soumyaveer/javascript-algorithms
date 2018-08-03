c779c4364dff93d515ed5ea40100f177a8caae86
Soumya Veer <veer.soumya@gmail.com>
2018-08-02 23:36:16 -0700

    add tests for clear function

--- tests/containers/stack.test.js
+++ tests/containers/stack.test.js
@@ -28,7 +28,7 @@ test('peek returns the top element from the stack', () => {
   expect(stack.peek()).toBe(2);
 });
 
-test('isEmpty returns true if stack has no elements', () =>{
+test('isEmpty returns true if stack has no elements', () => {
   let stack = new Stack();
 
   expect(stack.isEmpty()).toBe(true)
@@ -41,4 +41,17 @@ test('isEmpty returns false if stack has elements', () => {
   stack.push(2);
 
   expect(stack.isEmpty()).toBe(false)
+});
+
+test('clear removes all elements from the stach', () => {
+  let stack = new Stack();
+
+  stack.push(1);
+  stack.push(2);
+
+  expect(stack.isEmpty()).toBe(false);
+
+  stack.clear();
+
+  expect(stack.isEmpty()).toBe(true);
 });
