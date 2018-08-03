ed6b6b09ad4d4455c3ed4b6b226cb95b10823080
Soumya Veer <veer.soumya@gmail.com>
2018-08-02 23:31:52 -0700

    add test for pop function

--- tests/containers/stack.test.js
+++ tests/containers/stack.test.js
@@ -8,3 +8,13 @@ test('push adds element at the top of the stack', () => {
   expect(stack.peek()).toBe(1);
 });
 
+test('pop removes element from the top of the stack', () =>{
+  let stack = new Stack();
+
+  stack.push(1);
+  stack.push(2);
+
+  expect(stack.size()).toBe(2);
+  expect(stack.pop()).toBe(2);
+  expect(stack.size()).toBe(1);
+});
