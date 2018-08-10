fd242610aba44eebbc9831f7133d3c4e0cd7eec9
Soumya Veer <veer.soumya@gmail.com>
2018-08-09 17:34:49 -0700

    add clear method to Queue#Class

--- src/containers/queue.js
+++ src/containers/queue.js
@@ -5,17 +5,17 @@ class Queue {
 
   //adds a new item at the back of the queue
   enqueue(element) {
-    this.items.push(element)
+    this.items.push(element);
   }
 
   // returns first element of the queue
   front(){
-    return this.items[0]
+    return this.items[0];
   }
 
   //removes first element from the queue
   dequeue(){
-    return this.items.shift()
+    return this.items.shift();
   }
 
   //returns the number of elements in the queue
@@ -26,6 +26,11 @@ class Queue {
   //returns true is queue has no elements otherwise false
   isEmpty(){
     return this.items.length === 0;
+  }
+
+  //clears the queue and returns an empty queue
+  clear(){
+    this.items = [];
   }
 }
 
