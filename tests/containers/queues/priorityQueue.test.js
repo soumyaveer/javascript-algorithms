f5f67ab41e7a6865444468ad7c400534c142ed3c
Soumya Veer <veer.soumya@gmail.com>
2018-08-10 10:18:03 -0700

    add test for front function and fix tests for dequeue and enqueue function of PriorityQueue

--- tests/containers/queues/priorityQueue.test.js
+++ tests/containers/queues/priorityQueue.test.js
@@ -9,7 +9,7 @@ test('QueueElement creates queue element and priority', () => {
 
   expect(priorityQueue.isEmpty()).toBe(false);
 
-  expect(priorityQueue.front().element).toBe("Jack")
+  expect(priorityQueue.front()).toEqual({"element": "Jack", "priority": 1})
 });
 
 test('clear removes all elements from the queue', () => {
@@ -32,7 +32,16 @@ test('dequeue removes first item from the queue', () =>{
   priorityQueue.enqueue("Jack", 1);
 
   expect(priorityQueue.size()).toBe(2);
-  expect(priorityQueue.dequeue().element).toBe("Jack");
+  expect(priorityQueue.dequeue()).toEqual({"element": "Jack", "priority": 1});
   expect(priorityQueue.size()).toBe(1);
+});
+
+test('front returns the first element from the queue', () => {
+  let priorityQueue = new PriorityQueue();
+
+  priorityQueue.enqueue("John", 2);
+  priorityQueue.enqueue("Jack", 1);
+
+  expect(priorityQueue.front()).toEqual({"element": "Jack", "priority": 1});
 });
 
