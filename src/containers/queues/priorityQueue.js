185ed3b65be99c645a02119b773819cec2ee5516
Soumya Veer <veer.soumya@gmail.com>
2018-08-09 17:50:42 -0700

    add enqueue method for priority queue

--- src/containers/queues/priorityQueue.js
+++ src/containers/queues/priorityQueue.js
@@ -8,5 +8,22 @@ class PriorityQueue{
     this.priority = priority;
   }
 
+  enqueue(element, priority){
+    let queueElement = new this.QueueElement(element, priority);
+    let added = false;
+
+    for(let i = 0; i < this.items.length - 1; i++){
+      if(queueElement.priority < this,items[i].priority){
+        this.items.splice(i, 0, queueElement);
+        added = true;
+        break;
+      }
+    }
+    if (!added){
+      this,items.push(queueElement);
+    }
+  };
+
+
 }
 module.exports = PriorityQueue;
--- tests/containers/queues/priorityQueue.test.js
+++ tests/containers/queues/priorityQueue.test.js
@@ -3,5 +3,4 @@ const PriorityQueue = require('../../../src/containers/queues/priorityQueue');
 test('QueueElement creates queue element and priority', () => {
   let priorityQueue = new PriorityQueue();
 
-
 });
