0f2ff00d5b13198eabbef31cc5d9673a91109aeb
Soumya Veer <veer.soumya@gmail.com>
2018-08-09 17:45:56 -0700

    add QueueElement method to PriorityQueue#Class

--- src/containers/queues/priorityQueue.js
+++ src/containers/queues/priorityQueue.js
@@ -1,4 +1,12 @@
 class PriorityQueue{
+  constructor(){
+    this.items = [];
+  }
+
+  QueueElement(element, priority){
+    this.element = element;
+    this.priority = priority;
+  }
 
 }
 module.exports = PriorityQueue;
--- tests/containers/queues/priorityQueue.test.js
+++ tests/containers/queues/priorityQueue.test.js
@@ -1,2 +1,7 @@
 const PriorityQueue = require('../../../src/containers/queues/priorityQueue');
 
+test('QueueElement creates queue element and priority', () => {
+  let priorityQueue = new PriorityQueue();
+
+
+});
