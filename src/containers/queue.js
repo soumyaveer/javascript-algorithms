859c3fd27070fbcabbdc3323f7d44b52de3c3466
Soumya Veer <veer.soumya@gmail.com>
2018-08-09 17:27:18 -0700

    add front and enqueue functions to Queue#Class

--- src/containers/queue.js
+++ src/containers/queue.js
@@ -2,7 +2,16 @@ class Queue {
   constructor(){
     this.items = []
   }
-  
+
+  //adds a new item at the back of the queue
+  enqueue(element) {
+    this.items.push(element)
+  }
+
+  front(){
+    return this.items[0]
+  }
+
 }
 
 module.exports = Queue;
