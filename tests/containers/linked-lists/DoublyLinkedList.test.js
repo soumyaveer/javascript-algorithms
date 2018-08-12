6678486cb13d2a71d6a0b98f6a5d2a7c2db8cba8
Soumya Veer <veer.soumya@gmail.com>
2018-08-12 15:31:42 -0700

    add tests for append function of DoublyLinkedList

--- tests/containers/linked-lists/DoublyLinkedList.test.js
+++ tests/containers/linked-lists/DoublyLinkedList.test.js
@@ -1,1 +1,20 @@
 const DoublyLinkedList = require('../../../src/containers/linked-lists/DoublyLinkedList');
+
+test('append adds a new item at the head of an empty list', () => {
+  let doublyLinkedList = new DoublyLinkedList();
+
+  expect(doublyLinkedList.isEmpty()).toBe(true);
+
+  doublyLinkedList.append(1);
+
+  expect(doublyLinkedList.size()).toBe(1);
+});
+
+
+test('append adds a new item to the end of the list', () => {
+  let doublyLinkedList = new DoublyLinkedList();
+  doublyLinkedList.append(1);
+  doublyLinkedList.append(2);
+
+  expect(doublyLinkedList.size()).toBe(2);
+});
