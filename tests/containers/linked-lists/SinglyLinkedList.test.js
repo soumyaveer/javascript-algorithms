6b4fa65b0e6055961776d56a6d7b72b2874793d3
Soumya Veer <veer.soumya@gmail.com>
2018-08-11 22:13:41 -0700

    add remove function test to SinglyLinkedList#Class

--- tests/containers/linked-lists/SinglyLinkedList.test.js
+++ tests/containers/linked-lists/SinglyLinkedList.test.js
@@ -15,10 +15,25 @@ test('insert adds a new item at a specified position in the list', () => {
   singlyLinkedList.append(4);
   singlyLinkedList.append(5);
 
+  expect(singlyLinkedList.size()).toBe(4);
+
   singlyLinkedList.insert(1, 2);
 
   expect(singlyLinkedList.size()).toBe(5);
-  expect(singlyLinkedList.next).toBe(2);
+  expect(singlyLinkedList.indexOf(2)).toBe(1);
 });
 
+test('remove deletes an item from the list', () => {
+  let singlyLinkedList = new SinglyLinkedList(1);
+  singlyLinkedList.append(2);
+  singlyLinkedList.append(3);
+  singlyLinkedList.append(4);
+
+  expect(singlyLinkedList.size()).toBe(4);
+
+  singlyLinkedList.remove(2);
+
+  expect(singlyLinkedList.size()).toBe(3);
+  expect(singlyLinkedList.indexOf(3)).toBe(1);
+});
 
