81542393fe1707732f0d4893f21be9581d74b70f
Soumya Veer <veer.soumya@gmail.com>
2018-08-11 22:09:58 -0700

    add tests for append and insert method

--- tests/containers/linked-lists/SinglyLinkedList.test.js
+++ tests/containers/linked-lists/SinglyLinkedList.test.js
@@ -1,3 +1,24 @@
 const Node = require('../../../src/containers/linked-lists/Node');
 const SinglyLinkedList = require('../../../src/containers/linked-lists/SinglyLinkedList');
 
+test('append adds a new item to the end of the list', () => {
+  let singlyLinkedList = new SinglyLinkedList(1);
+
+  singlyLinkedList.append(2);
+
+  expect(singlyLinkedList.size()).toBe(2)
+});
+
+test('insert adds a new item at a specified position in the list', () => {
+  let singlyLinkedList = new SinglyLinkedList(1);
+  singlyLinkedList.append(3);
+  singlyLinkedList.append(4);
+  singlyLinkedList.append(5);
+
+  singlyLinkedList.insert(1, 2);
+
+  expect(singlyLinkedList.size()).toBe(5);
+  expect(singlyLinkedList.next).toBe(2);
+});
+
+
