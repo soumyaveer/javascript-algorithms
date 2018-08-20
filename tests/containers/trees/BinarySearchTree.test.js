751c71d71946a46d0b4968f4b868611f729577e6
Soumya Veer <veer.soumya@gmail.com>
2018-08-20 12:43:50 -0700

    add tests for search() method of BinarySearchTree

--- tests/containers/trees/BinarySearchTree.test.js
+++ tests/containers/trees/BinarySearchTree.test.js
@@ -4,7 +4,7 @@ const BinarySearchTree = require('../../../src/containers/trees/BinarySearchTree
 describe('insert()', () =>{
   test('if tree is empty, inserts key at root position', () => {
     const tree = new BinarySearchTree();
-    tree.insert(11)
+    tree.insert(11);
 
     expect(tree.getRoot().key).toBe(11);
   });
@@ -20,3 +20,23 @@ describe('insert()', () =>{
     expect(root.right.key).toBe(15);
   });
 });
+
+describe('search()', () =>{
+  test('returns true if key is present in the tree', () => {
+    const tree = new BinarySearchTree()
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+
+    expect(tree.search(7)).toBe(true);
+  });
+
+  test('returns false if key is not present in the tree', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+
+    expect(tree.search(12)).toBe(false);
+  })
+})
