aa2cb78fe7a5f7cc68a86886956de3a413086105
Soumya Veer <veer.soumya@gmail.com>
2018-08-20 12:54:38 -0700

    add test for post-order-traversal for BinarySearchTree

--- tests/containers/trees/BinarySearchTree.test.js
+++ tests/containers/trees/BinarySearchTree.test.js
@@ -81,6 +81,28 @@ describe('preOrderTraverse()', () => {
     tree.insert(18);
     tree.insert(25);
 
-    expect(tree.preOrderTraversal()).toBe("11 7 5 3 6 9 8 10 15 13 12 14 20 18 25");
+    expect(tree.preOrderTraverse()).toBe("11 7 5 3 6 9 8 10 15 13 12 14 20 18 25");
+  });
+});
+
+describe('postOrderTraverse()', () => {
+  test('visits all node of the tree using post-order-traversal', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+
+    expect(tree.postOrderTraverse()).toBe("3 6 5 8 10 9 7 12 14 13 18 25 20 15 11");
   });
 });
