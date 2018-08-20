062283766ce9574b13a1e711e40e623440175472
Soumya Veer <veer.soumya@gmail.com>
2018-08-20 12:52:00 -0700

    add test for pre-order-traversal BinarySearchTree

--- tests/containers/trees/BinarySearchTree.test.js
+++ tests/containers/trees/BinarySearchTree.test.js
@@ -62,3 +62,25 @@ describe('inOrderTraverse()', () => {
     expect(tree.inOrderTraversal()).toBe("3 5 6 7 8 10 11 12 13 14 15 18 20 25");
   });
 });
+
+describe('preOrderTraverse()', () => {
+  test('visits all node of the tree using pre-order-traversal', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+
+    expect(tree.preOrderTraversal()).toBe("11 7 5 3 6 9 8 10 15 13 12 14 20 18 25");
+  });
+});
