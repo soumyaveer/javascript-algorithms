bd50210104f87124503f0ec20d065dcf45ca7f9e
Soumya Veer <veer.soumya@gmail.com>
2018-08-20 12:50:52 -0700

    add test for in-order-traversal for BinarySearchTree

--- tests/containers/trees/BinarySearchTree.test.js
+++ tests/containers/trees/BinarySearchTree.test.js
@@ -38,5 +38,27 @@ describe('search()', () =>{
     tree.insert(15);
 
     expect(tree.search(12)).toBe(false);
-  })
-})
+  });
+});
+
+describe('inOrderTraverse()', () => {
+  test('visits all node of the tree using in-order-traversal', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+
+    expect(tree.inOrderTraversal()).toBe("3 5 6 7 8 10 11 12 13 14 15 18 20 25");
+  });
+});
