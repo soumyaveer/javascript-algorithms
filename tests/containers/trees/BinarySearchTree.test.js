c4ff26826506213dda8ecff633e6bddbd366d328
Soumya Veer <veer.soumya@gmail.com>
2018-08-20 12:40:00 -0700

    add tests for insert method of BinarySearchTree

--- tests/containers/trees/BinarySearchTree.test.js
+++ tests/containers/trees/BinarySearchTree.test.js
@@ -1,3 +1,22 @@
 const Node = require('../../../src/containers/trees/Node');
 const BinarySearchTree = require('../../../src/containers/trees/BinarySearchTree');
 
+describe('insert()', () =>{
+  test('if tree is empty, inserts key at root position', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11)
+
+    expect(tree.getRoot().key).toBe(11);
+  });
+
+  test('inserts new key in the tree', () => {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+
+    const root = tree.getRoot();
+    expect(root.left.key).toBe(7);
+    expect(root.right.key).toBe(15);
+  });
+});
