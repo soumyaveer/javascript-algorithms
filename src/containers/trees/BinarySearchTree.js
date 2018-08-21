51048a9f760fe3ee18836d5860794fda355e6a56
Soumya Veer <veer.soumya@gmail.com>
2018-08-21 14:18:08 -0700

    add insert method to BinarySearchTree

--- src/containers/trees/BinarySearchTree.js
+++ src/containers/trees/BinarySearchTree.js
@@ -1,7 +1,41 @@
 const Node = require('../../../src/containers/trees/Node');
 
 class BinarySearchTree{
+  constructor(){
+    this.root = null;
+  };
 
+  // returns the root of the tree
+  getRoot(){
+    return this.root;
+  }
+
+  // inserts a new key to the tree
+  insert(key){
+    let newNode = new Node(key);
+
+    if(this.root === null){
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode)
+    }
+  }
+
+  insertNode(node, newNode){
+    if(newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+    } else {
+      if(node.right === null) {
+        node.right = newNode;
+      } else {
+        insertNode(node.right, newNode)
+      }
+    }
+  }
 }
 
 module.exports = BinarySearchTree;
