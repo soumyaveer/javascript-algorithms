6cf26d35e9e564c0c2f5f70fa84bcd3efddaee54
Soumya Veer <veer.soumya@gmail.com>
2018-08-27 16:54:24 -0700

    add constructor and getRoot method to AVLTree

--- src/containers/trees/AVLTree.js
+++ src/containers/trees/AVLTree.js
@@ -1,1 +1,14 @@
 const Node = require('../../../src/containers/trees/Node');
+
+class AVLTree{
+  constructor(){
+    this.root = null;
+  }
+
+  // returns the root of the tree
+  getRoot(){
+    return this.root;
+  }
+}
+
+module.exports = AVLTree;
