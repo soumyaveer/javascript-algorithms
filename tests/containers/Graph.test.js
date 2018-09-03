50f17848645d4545bf52f2c6e28a59937e61fa8e
Soumya Veer <veer.soumya@gmail.com>
2018-09-03 14:55:23 -0700

    add test for addVertex method of Graph

--- tests/containers/Graph.test.js
+++ tests/containers/Graph.test.js
@@ -1,1 +1,15 @@
 const Graph = require('../../src/containers/Graph');
+
+describe('addVertex', () => {
+  test('adds new vertex to the graph', () => {
+    let graph = new Graph();
+    let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+    for(let i = 0; i < vertices.length; i++){
+      graph.addVertex(vertices[i]);
+    }
+
+    expect(graph.isEmpty()).toBe(false);
+    expect(graph.numberOfVertices()).toBe(9);
+  });
+});
