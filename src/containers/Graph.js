31401a5668ef2185bff2a731206ed51ce52f978f
Soumya Veer <veer.soumya@gmail.com>
2018-09-03 15:09:15 -0700

    add isEmpty and numberOfVertices methods and related tests to Graph

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -1,5 +1,20 @@
-class Graph{
+const Dictionary = require('../../src/containers/Dictionary');
 
+class Graph{
+  constructor(){
+    this.vertices = [];
+    this.adjList = new Dictionary();
+  }
+
+  //returns the number of vertices in the graph
+  numberOfVertices(){
+    return this.vertices.length;
+  }
+
+  // returns true if graph is empty
+  isEmpty(){
+    return !this.numberOfVertices();
+  }
 }
 
 module.exports = Graph;
--- tests/containers/Graph.test.js
+++ tests/containers/Graph.test.js
@@ -1,5 +1,43 @@
 const Graph = require('../../src/containers/Graph');
 
+describe('numberOfVertices', () => {
+  test('returns the number of vertices of the graph', () => {
+    let graph = new Graph();
+
+    expect(graph.numberOfVertices()).toBe(0);
+
+    let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+    for(let i = 0; i < vertices.length; i++){
+      graph.addVertex(vertices[i]);
+    }
+
+    expect(graph.numberOfVertices()).toBe(9);
+  });
+});
+
+describe('isEmpty', () => {
+  test('returns true if graph is empty', () => {
+    let graph = new Graph();
+
+    expect(graph.isEmpty()).toBe(true);
+  });
+
+  test('returns false if graph is not empty', () => {
+    let graph = new Graph();
+
+    expect(graph.isEmpty()).toBe(true);
+
+    let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+    for(let i = 0; i < vertices.length; i++){
+      graph.addVertex(vertices[i]);
+    }
+
+    expect(graph.isEmpty()).toBe(false);
+  });
+});
+
 describe('addVertex', () => {
   test('adds new vertex to the graph', () => {
     let graph = new Graph();
