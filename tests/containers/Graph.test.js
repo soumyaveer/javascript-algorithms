05972b0bb4c055bce5c910416d5c64cd3c443c5d
Soumya Veer <veer.soumya@gmail.com>
2018-09-03 15:00:47 -0700

    add test for addEdge method of Graph#Class

--- tests/containers/Graph.test.js
+++ tests/containers/Graph.test.js
@@ -13,3 +13,22 @@ describe('addVertex', () => {
     expect(graph.numberOfVertices()).toBe(9);
   });
 });
+
+describe('addEdge', () => {
+  test('adds edges to the vertices', () => {
+    let graph = new Graph();
+
+    let vertices = ['A', 'B', 'C'];
+
+    for(let i = 0; i < vertices.length; i++){
+      graph.addVertex(vertices[i]);
+    }
+
+    graph.addEdge('A', 'B');
+    graph.addEdge('A', 'C');
+
+    expect(graph.isEmpty()).toBe(false);
+    expect(graph.numberOfVertices()).toBe(3);
+    expect(graph.toString()).toBe("A -> B C \nB -> A \nC -> A\n");
+  });
+});
