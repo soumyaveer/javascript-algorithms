093f0cbe1347fbf1a9da04e82a138159ad5d5da2
Soumya Veer <veer.soumya@gmail.com>
2018-09-13 14:39:21 -0700

    add time complexities to BubbleSort and SelectionSort

--- src/algorithms/sorting/BubbleSort.js
+++ src/algorithms/sorting/BubbleSort.js
@@ -4,14 +4,14 @@ class BubbleSort{
   }
 
   sort(){
-    let length = this.elements.length;
+    let length = this.elements.length;                //O(1)
 
-    for(let i = 0; i < length; i++){
-      for(let j = 0; j < length - 1; j++){
-        if(this.elements[j] > this.elements[j+1]){
-          let temp = this.elements[j];
-          this.elements[j] = this.elements[j+1];
-          this.elements[j+1] = temp;
+    for(let i = 0; i < length; i++){                  //O(n)
+      for(let j = 0; j < length - 1; j++){            //O(n - 1)
+        if(this.elements[j] > this.elements[j+1]){    //O(1)
+          let temp = this.elements[j];                //O(1)
+          this.elements[j] = this.elements[j+1];      //O(1)
+          this.elements[j+1] = temp;                  //O(1)
         }
       }
     }
@@ -20,3 +20,11 @@ class BubbleSort{
 }
 
 module.exports = BubbleSort;
+
+/* Calculating Time complexity:
+
+  F(n) = O(n) * O(n - 1) * O(1) + O(1) + O(1) + O(1) + O(1)
+       = O(n^2 - n) * O(1)
+  Discarding everything other than the highest value.
+  F(n) = O(n^2)
+ */
--- src/algorithms/sorting/SelectionSort.js
+++ src/algorithms/sorting/SelectionSort.js
@@ -5,19 +5,19 @@ class SelectionSort{
 
   sort(){
     let minIndex;
-    let length = this.elements.length;
-    for(let i = 0; i < length - 1; i++){
-      minIndex = i;
-      for(let j = i; j< length; j++){
-        if(this.elements[minIndex] > this.elements[j]){
-          minIndex = j;
+    let length = this.elements.length;                    //O(1)
+    for(let i = 0; i < length - 1; i++){                  //O(n-1)
+      minIndex = i;                                       //O(1)
+      for(let j = i; j< length; j++){                     //O(n)
+        if(this.elements[minIndex] > this.elements[j]){   //O(1)
+          minIndex = j;                                   //O(1)
         }
       }
 
-      if(i !== minIndex){
-        let temp = this.elements[minIndex];
-        this.elements[minIndex] = this.elements[i];
-        this.elements[i] = temp;
+      if(i !== minIndex){                                 //O(1)
+        let temp = this.elements[minIndex];               //O(1)
+        this.elements[minIndex] = this.elements[i];       //O(1)
+        this.elements[i] = temp;                          //O(1)
       }
     }
 
@@ -26,3 +26,10 @@ class SelectionSort{
 }
 
 module.exports = SelectionSort;
+
+/* Calculating Time complexity:
+  F(n) = O(n) * O(n - 1) * O(1) + O(1) + O(1) + O(1) + O(1) + O(1) + O(1) + O(1)
+       = O(n^2 - n) * O(1)
+  Discarding everything other than the highest value.
+  F(n) = O(n^2)
+ */
