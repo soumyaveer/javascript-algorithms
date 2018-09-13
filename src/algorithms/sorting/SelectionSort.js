3abd0b500e6e33c6bf59aca4fc64da57fef9f53b
Soumya Veer <veer.soumya@gmail.com>
2018-09-13 14:19:02 -0700

    add sort method for SelectionSort

--- src/algorithms/sorting/SelectionSort.js
+++ src/algorithms/sorting/SelectionSort.js
@@ -1,5 +1,28 @@
 class SelectionSort{
+  constructor(elements){
+    this.elements = elements;
+  }
 
+  sort(){
+    let minIndex;
+    let length = this.elements.length;
+    for(let i = 0; i < length - 1; i++){
+      minIndex = i;
+      for(let j = i; j< length; j++){
+        if(this.elements[minIndex] > this.elements[j]){
+          minIndex = j;
+        }
+      }
+
+      if(i !== minIndex){
+        let temp = this.elements[minIndex];
+        this.elements[minIndex] = this.elements[i];
+        this.elements[i] = temp;
+      }
+    }
+
+    return this.elements;
+  }
 }
 
 module.exports = SelectionSort;
