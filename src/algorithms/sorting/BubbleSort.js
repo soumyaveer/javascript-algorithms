e868ccb16bcb78e74d010acb8beb494bfd918af5
Soumya Veer <veer.soumya@gmail.com>
2018-09-13 12:37:35 -0700

    add sort method to BubbleSort and add related tests

--- src/algorithms/sorting/BubbleSort.js
+++ src/algorithms/sorting/BubbleSort.js
@@ -1,5 +1,23 @@
 class BubbleSort{
+  constructor(elements){
+    this.elements = elements;
+  }
 
+  sort(){
+    let length = this.elements.length;
+
+    for(let i = 0; i < length; i++){
+      for(let j = 0; j < length - 1; j++){
+        if(this.elements[j] > this.elements[j+1]){
+          let temp = this.elements[j];
+          this.elements[j] = this.elements[j+1];
+          this.elements[j+1] = temp;
+        }
+      }
+    }
+
+    return this.elements
+  }
 }
 
 module.exports = BubbleSort;
