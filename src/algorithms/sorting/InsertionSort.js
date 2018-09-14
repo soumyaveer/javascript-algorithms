b0e165705f09813c11b765d0eec530448ac5b7be
Soumya Veer <veer.soumya@gmail.com>
2018-09-13 21:43:58 -0700

    add sort method to InsertionSort

--- src/algorithms/sorting/InsertionSort.js
+++ src/algorithms/sorting/InsertionSort.js
@@ -2,6 +2,22 @@ class InsertionSort{
   constructor(elements){
     this.elements = elements;
   }
+
+  sort(){
+    let length = this.elements.length;
+    let temp, j;
+
+    for(let i = 0; i < length; i++){
+      temp = this.elements[i];
+      j = i;
+      while(j > 0 && this.elements[j - 1] > temp){
+        this.elements[j] = this.elements[j-1];
+        j--;
+      }
+      this.elements[j] = temp;
+    }
+    return this.elements;
+  }
 }
 
 module.exports = InsertionSort;
