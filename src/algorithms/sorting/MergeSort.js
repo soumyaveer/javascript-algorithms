b033041331c7f1580a8bacffc3034a987f92794e
Soumya Veer <veer.soumya@gmail.com>
2018-09-14 13:46:50 -0700

    add tests for MergeSort

---
+++ src/algorithms/sorting/MergeSort.js
@@ -0,0 +1,5 @@
+class MergeSort{
+  
+}
+
+module.exports = MergeSort;
---
+++ tests/algorithms/sorting/MergeSort.test.js
@@ -0,0 +1,31 @@
+const MergeSort = require('../../../src/algorithms/sorting/MergeSort');
+
+describe('sort', () => {
+  test('sorts the elements in ascending order when all numbers are positive', () => {
+    let elements = [0, 2, 5, 3, 1, 4];
+    let merge = new MergeSort(elements);
+
+    expect(merge.sort()).toMatchObject([0, 1, 2, 3, 4, 5]);
+  });
+
+  test('sorts the elements in ascending order when all numbers are not positive', () => {
+    let elements = [0, 2, -5, 3, -1, 4];
+    let merge = new MergeSort(elements);
+
+    expect(merge.sort()).toMatchObject([-5, -1, 0, 2, 3, 4]);
+  });
+
+  test('sorts the elements in ascending order when all numbers are negative', () => {
+    let elements = [-2, -5, -3, -1, -4];
+    let merge = new MergeSort(elements);
+
+    expect(merge.sort()).toMatchObject([-5, -4, -3, -2, -1]);
+  });
+
+  test('sorts the elements in ascending order when one number is infinity', () => {
+    let elements = [-2, Infinity, -3, -1, -4];
+    let merge = new MergeSort(elements);
+
+    expect(merge.sort()).toMatchObject([-4, -3, -2, -1, Infinity]);
+  });
+});
