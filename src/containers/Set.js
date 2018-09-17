413a6e3fbf1c29750d976e73c291121615b0cbf3
Soumya Veer <veer.soumya@gmail.com>
2018-09-17 12:02:52 -0700

    add tests for add method of Set data structure

--- src/containers/Set.js
+++ src/containers/Set.js
@@ -1,3 +1,7 @@
+/* Set: is a collection of items that are unordered and consists of unique elements.
+   Null Set: A set with no element is called null set or empty set.
+ */
+
 class Set{
 
 }
--- tests/containers/Set.test.js
+++ tests/containers/Set.test.js
@@ -1,2 +1,24 @@
 const Set = require("../../src/containers/Set");
 
+describe('add', () => {
+  test('adds a new item to the set, when set is empty', () => {
+    let set = new Set();
+
+    expect(set.size()).toBe(0);
+
+    set.add(1);
+
+    expect(set.size()).toBe(1);
+    expect(set.has(1)).toBe(true);
+  });
+
+  test('adds a new item to the set, when set is not empty', () => {
+    let set = new Set();
+    set.add(1);
+    set.add(2);
+    set.add(3);
+
+    expect(set.size()).toBe(3);
+    expect(set.values()).toMatchObject(["1", "2", "3"]);
+  });
+});
