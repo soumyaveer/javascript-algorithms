0d0b55f6dd93e0da6118bc726acfdf8daa60ace8
Soumya Veer <veer.soumya@gmail.com>
2018-09-16 17:54:02 -0700

    add algorithm as comment in SequentialSearch

--- src/algorithms/searching/SequentialSearch.js
+++ src/algorithms/searching/SequentialSearch.js
@@ -1,5 +1,15 @@
 class SequentialSearch{
+  constructor(elements){
+    this.elements = elements;
+  }
 
+  /* Algorithm: Loop through each element in an array, if element searched is found return true otherwise return false.
+     search(searchKey)
+     1. loop from 0 to n, n being the length of the array
+        1.1 if any element in array equals searchKey
+            1.1.1 return true
+     2. return false
+   */
 }
 
 module.exports = SequentialSearch;
