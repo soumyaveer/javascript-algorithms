9bf87dbc1c1643bbe08f3d621fb79a0955aca9a3
Soumya Veer <veer.soumya@gmail.com>
2018-09-17 12:30:05 -0700

    add add method to Set

--- src/containers/Set.js
+++ src/containers/Set.js
@@ -10,6 +10,19 @@ class Set{
   has(value){
     return this.items.hasOwnProperty(value);
   }
+
+  /* add: adds the value to the set
+    1. check if the value is already present in the set.
+       1.1 if no, add value to the set and return true
+    2. return false
+   */
+  add(value){
+    if(!this.has(value)){
+      this.items[value] = value;
+      return true;
+    }
+    return false;
+  }
 }
 
 module.exports = Set;
