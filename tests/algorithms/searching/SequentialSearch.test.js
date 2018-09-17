10e95bd0a9afadbff31bc1f7a0edc6f79005c9fd
Soumya Veer <veer.soumya@gmail.com>
2018-09-16 17:50:28 -0700

    add tests for sequential search

--- tests/algorithms/searching/SequentialSearch.test.js
+++ tests/algorithms/searching/SequentialSearch.test.js
@@ -1,2 +1,19 @@
 const SequentialSearch = require('../../../src/algorithms/searching/SequentialSearch');
 
+describe('search', () => {
+  test('returns true if searched item is present in the array', () => {
+    let elements = [0, 2, 5, 3, 1, 4];
+    let searchKey = 3;
+    let sequential = new SequentialSearch(elements);
+
+    expect(sequential.search(searchKey)).toBe(true);
+  });
+
+  test('returns false if searched item is not present in the array', () => {
+    let elements = [0, 2, 5, 3, 1, 4];
+    let searchKey = 8;
+    let sequential = new SequentialSearch(elements);
+
+    expect(sequential.search(searchKey)).toBe(false);
+  });
+});
