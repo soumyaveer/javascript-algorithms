b4de9b614d7f68d583e82fde7698274baca6a2ca
Soumya Veer <veer.soumya@gmail.com>
2018-09-17 13:06:59 -0700

    add tests for intersection method of Set data structure

--- tests/containers/Set.test.js
+++ tests/containers/Set.test.js
@@ -194,3 +194,35 @@ describe('union', () => {
     expect(unionAB.values()).toMatchObject([4, 5, 6]);
   });
 });
+
+describe('intersection', () =>{
+  test('returns the common elements between two sets', () => {
+    let setA = new Set();
+    setA.add(1);
+    setA.add(2);
+    setA.add(3);
+
+    let setB = new Set();
+    setB.add(2);
+    setB.add(3);
+    setB.add(4);
+
+    let intersectionAB = setA.union(setB);
+    expect(intersectionAB.values()).toMatchObject([2, 3]);
+  });
+
+  test('returns a null set if no elements are common between two sets', () => {
+    let setA = new Set();
+    setA.add(1);
+    setA.add(2);
+    setA.add(3);
+
+    let setB = new Set();
+    setB.add(4);
+    setB.add(5);
+    setB.add(6);
+
+    let intersectionAB = setA.union(setB);
+    expect(intersectionAB.values()).toMatchObject([]);
+  });
+});
