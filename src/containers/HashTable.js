adf1c5b966eb325c1fa9221f1e76ac0475be3427
Soumya Veer <veer.soumya@gmail.com>
2018-09-18 13:03:18 -0700

    add hashCode method to HashTable

--- src/containers/HashTable.js
+++ src/containers/HashTable.js
@@ -1,5 +1,16 @@
 class HashTable{
+  constructor(){
+    this.table = [];
+  }
 
+  hashCode(key){
+    let hash = 0;
+
+    for(let i = 0; i < key.length; i++){
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
 }
 
 module.exports = HashTable;
