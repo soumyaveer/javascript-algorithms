6b400825f19b1838cf2f9b4b34c05dccf6d880c6
Soumya Veer <veer.soumya@gmail.com>
2018-09-18 12:56:29 -0700

    add set method test to HashTable

--- tests/containers/HashTable.test.js
+++ tests/containers/HashTable.test.js
@@ -1,2 +1,15 @@
 let HashTable = require("../../src/containers/HashTable");
 
+describe('set', () => {
+  test('adds a new item to the hash table', () => {
+    let hash = new HashTable();
+
+    hash.set('Gandalf', 'gandalf@email.com');
+    hash.set('Legolas', 'legolas@elf.com');
+    hash.set('Aragorn', 'strider@gondor.com');
+    hash.set('Frodo', 'frodo_baggins@shire.com');
+
+    expect(hash.get('Gandalf')).toBe('gandalf@email.com');
+    expect(hash.get('Frodo')).toBe('frodo_baggins@shire.com');
+  });
+});
