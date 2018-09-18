e0f72453f39a157d714a9ed09a11caf487eb79bf
Soumya Veer <veer.soumya@gmail.com>
2018-09-18 12:57:32 -0700

    add test for get method of HashTable

--- tests/containers/HashTable.test.js
+++ tests/containers/HashTable.test.js
@@ -13,3 +13,17 @@ describe('set', () => {
     expect(hash.get('Frodo')).toBe('frodo_baggins@shire.com');
   });
 });
+
+describe('get', () => {
+  test('returns the value from the hash table using key', () => {
+    let hash = new HashTable();
+
+    hash.set('Gandalf', 'gandalf@email.com');
+    hash.set('Legolas', 'legolas@elf.com');
+    hash.set('Aragorn', 'strider@gondor.com');
+    hash.set('Frodo', 'frodo_baggins@shire.com');
+
+    expect(hash.get('Gandalf')).toBe('gandalf@email.com');
+    expect(hash.get('Frodo')).toBe('frodo_baggins@shire.com');
+  });
+});
